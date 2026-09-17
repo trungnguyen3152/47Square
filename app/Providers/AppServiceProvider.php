bc1391b737e69df573418e193307486228f7d9b9
47Square Admin <admin@47square.vn>
2026-09-18 02:42:38 +0700

    Fix Vercel storage path and add LOG_CHANNEL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (isset($_ENV['VERCEL_URL'])) {
-            $this->app->useStoragePath('/tmp/storage');
-        }
+        //
     }
 }
