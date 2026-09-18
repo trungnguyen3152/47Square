8869099f4c9aaa3849653fbf5186f19fd5db401a
47Square Admin <admin@47square.vn>
2026-09-18 18:53:51 +0700

    Redirect root to coming-soon page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/coming-soon',
+        permanent: false, // Để false để sau này bạn ra mắt web thật thì Google không nhớ cache
+      },
+    ]
+  },
+};
 
 export default nextConfig;
